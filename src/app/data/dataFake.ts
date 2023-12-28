c5e8fa58db6ffad03841505be1c09006bb6d601b
Arthur Saldanha <arthursaldanha101@gmail.com>
2023-12-27 23:47:45 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "Novo filme do Homem de ferro anunciado",
+        "description": "marvel anuncia um novo filme do homem de ferro, confira",
+        "photoCover": "https://s2-techtudo.glbimg.com/1itp07Es0lKmVBlduUYMyTMtI8Q=/0x0:1200x800/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/0/l/zczH0xSDWDb3jdVnTyHg/58.png"
+    },
+    {
+        "id": "2",
+        "title": "Nova série da marvel anunciada",
+        "description": "bla bla bla bla bla",
+        "photoCover": "https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/10/29/1451612854-8ffe78addae85f28880875f37957b143.png"
+    }
+];
